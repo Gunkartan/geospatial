a5fc9c1678b56d8a33bf314bc006e0b392626671
Karun Tancharoen <149927709+Gunkartan@users.noreply.github.com>
2026-09-01 23:56:34 +0700

    feat(code): add pipeline building preprocessing

---
+++ code/preprocessing/preprocess_buildings_final_pipeline.py
@@ -0,0 +1,25 @@
+import sys
+import numpy as np
+import pandas as pd
+
+def preprocess_buildings(year: int) -> None:
+    df = pd.read_csv(f'../datasets/raw_buildings_final_pipeline_{year}.csv')
+    df = df.replace(
+        [
+            np.inf,
+            -np.inf
+        ],
+        np.nan
+    )
+    df = df.dropna().reset_index(drop=True)
+    df.to_csv(
+        f'../datasets/preprocessed_buildings_final_pipeline_{year}.csv',
+        index=False
+    )
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError('Please provide a year')
+
+    year = int(sys.argv[1])
+    preprocess_buildings(year)
